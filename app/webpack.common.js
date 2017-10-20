c0ecdbcb99c720c774152097b4f43f4b4ef5bc30
Jonathan Lake <jwtlake@gmail.com>
2017-10-19 21:31:25 -0700

    webpack config refactor
    split config up by env
    dev now includes source-maps
    npm start auto opens browser
    updated .gitignore to include build and dist

--- app/webpack.common.js
+++ app/webpack.common.js
@@ -1,13 +1,9 @@
+const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
@@ -29,4 +25,8 @@ module.exports = {
     new CleanWebpackPlugin(['dist', 'build'], { verbose: true }),
     new HtmlWebpackPlugin({ template: './src/index.html' }),
   ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
